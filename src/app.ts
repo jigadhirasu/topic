dd0f21a4a44182cee92de706bb9f309c03353aa5
vince <vinceyang@simbo.com.tw>
2022-04-13 16:02:41 +0800

    Comment

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,18 @@ const fs = require('fs');
 const express = require("express");
 const showdown   = require('showdown');
 
-// default port to listen
+// Default port
 const port = 3000;
 
 const app = express();
 const converter = new showdown.Converter();
 
-// define a route handler for the default home page
 app.get("/", (req, res) => {
     const text = fs.readFileSync('./README.md').toString();
     const html = converter.makeHtml(text);
     res.send(html);
 });
 
-// start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
